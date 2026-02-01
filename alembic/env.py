d840251dd4ff460b2c2cdf1aa1de7bca8f04ecdd
GAKiknadze <gakiknadze@yandex.ru>
2026-02-02 00:47:56 +0300

    Updated ruff settings

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,6 @@ import asyncio
 from logging.config import fileConfig
 
 from alembic import context
-from pydantic.types import ImportString
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
@@ -20,16 +19,12 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
 config.set_main_option("sqlalchemy.url", Settings().database.url)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
